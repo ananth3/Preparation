13a7662bee9faf9ea6bc26386a3824fef8c2218c
ananth3 <35170699+ananth3@users.noreply.github.com>
2021-03-23 15:47:59 +0530

    Update running_sum_1darray.py

--- array/running_sum_1darray.py
+++ array/running_sum_1darray.py
@@ -1,3 +1,7 @@
+#Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i])
+#Input: nums = [1,2,3,4]
+#Output: [1,3,6,10]
+
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         res = [None]*len(nums)
