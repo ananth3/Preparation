c8f5d00043abf2fd540e91cdbdb9e1c4d945c420
ananth3 <35170699+ananth3@users.noreply.github.com>
2021-03-23 14:47:50 +0530

    Delete running_sum_1darray.py

--- running_sum_1darray.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        res = [None]*len(nums)
-        res[0] = nums[0]
-        for i in range(1,len(nums)):
-            res[i] = nums[i]+res[i-1]
-        return res
