959d8634aa3dd9b9dc6d490422da0705aa52864e
ananth3 <35170699+ananth3@users.noreply.github.com>
2021-03-23 15:56:02 +0530

    Update xor_operations_in_array.py

--- array/xor_operations_in_array.py
+++ array/xor_operations_in_array.py
@@ -1,3 +1,11 @@
+#Given an integer n and an integer start.
+#Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+#Return the bitwise XOR of all elements of nums.
+# Input: n = 5, start = 0
+# Output: 8
+# Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+
+
 class Solution:
     def xorOperation(self, n: int, start: int):
         x = 0
