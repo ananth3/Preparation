485d5540b4b5646e1318352c8b97092393de5516
ananth3 <35170699+ananth3@users.noreply.github.com>
2021-04-12 17:05:51 +0530

    Add files via upload

---
+++ strings/Reverse_String.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        j = len(s)-1
+        for i in range(len(s)):
+            if i<j:
+                s[i],s[j] = s[j],s[i]
+                j = j-1
